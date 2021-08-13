730b83a63ff354664ea8271f657dafb31b389d0d
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-13 16:55:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 echo 'echo $(expr $(cat ~/.sessions) + 1) > ~/.sessions' > .bashrc
 echo 'echo $(expr $(cat ~/.sessions) - 1) > ~/.sessions' > .bash_logout
-echo 0 > ~/.sessions
 mv env.example env
 echo 'setup completed'
