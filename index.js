90cf8ee1561dbcabdcdb261f1a608fb085339298
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-13 20:34:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const { Client } = require('discord.js-light'),
 	client = new Client(),
-	{ TOKEN } = process.env
+	{ TOKEN } = process.env,
+	{ execSync } = require("child_process");
 client.on("ready",()=>{
 	console.log(`selfbot is ready on ${client.user.tag}`);
 	function updateStatus(){
-        	client.user.setPresence({activity:{name:"Termux | Linux terminal environment for Android","state":`${require("child_process").execSync('printf $(cat ~/.sessions)')} session(s)`}});
+        	client.user.setPresence({activity:{name:"Termux | Linux terminal environment for Android","state":`${execSync('printf $(cat ~/.sessions)')} session(s)`}});
         }
         updateStatus();
         setInterval(updateStatus,20000);
