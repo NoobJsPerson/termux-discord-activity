248a0e369e771f4a114d0be91ac386875001c2ac
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-14 12:45:35 +0100

    update util.js

--- util.js
+++ util.js
@@ -1,8 +1,12 @@
 const { execSync } = require("child_process");
+function setActivity(){
+	
+}
 function setStatus(){
 	const obj = {
-		name: "Termux: Linux terminal environment for Android"
+		type: 0,
+		name: "Termux: Linux terminal environment for Android",
 		status: `${execSync("printf $(cat ~./sessions)")} session(s)`
-	}
+	};
 	
 }
