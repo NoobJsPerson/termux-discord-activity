79363dd5a7c669906bfbcf6464136b6dbfceaf3c
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-17 17:30:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-echo 'echo $(expr $(cat ~/.sessions) + 1) > ~/.sessions' > .bashrc
-echo 'echo $(expr $(cat ~/.sessions) - 1) > ~/.sessions' > .bash_logout
-echo 'echo $(expr $(cat ~/.sessions) + 1) > ~/.sessions' > .zshrc
-echo 'echo $(expr $(cat ~/.sessions) - 1) > ~/.sessions' > .zlogout
+login='echo $(expr $(cat ~/.sessions) + 1) > ~/.sessions'
+logout='echo $(expr $(cat ~/.sessions) - 1) > ~/.sessions'
+echo $login > .bashrc
+echo $logout > .bash_logout
+echo $login > .zshrc
+echo $logout > .zlogout
 mv .env.example .env
 echo 0 > .sessions
 echo 'setup completed'
