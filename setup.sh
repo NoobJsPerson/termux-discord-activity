31123ac7c58cc484a2d15832d3ab18eeec48faac
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-10-09 14:56:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 login='echo $(expr $(cat ~/.sessions) + 1) > ~/.sessions'
 logout='echo $(expr $(cat ~/.sessions) - 1) > ~/.sessions'
-echo $login > .bashrc
-echo $logout > .bash_logout
-echo $login > .zshrc
-echo $logout > .zlogout
+case $SHELL in
+  "/data/data/com.termux/files/usr/bin/bash") echo $login > .bashrc && echo $logout > .bash_logout ;;
+  "/data/data/com.termux/files/usr/bin/zsh") echo $login > .zshrc && echo $logout > .zlogout ;;
+esac
 mv .env.example .env
 echo 0 > .sessions
 echo 'setup completed'
